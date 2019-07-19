940c988e350a49796320a52152fdadd1d2fc5465
Matt Eby <eby.matthew@gmail.com>
2019-07-18 21:55:41 -0500

    clean up

--- p2_continuous-control/train.py
+++ p2_continuous-control/train.py
@@ -66,21 +66,19 @@ def train(env_location, curve_path, n_episodes=1000):
     def create_agent():
         return Agent(state_size=states.shape[1], action_size=brain.vector_action_space_size, random_seed=random_seed, memory=memory, batch_size=BATCH_SIZE)
 
-    agent = create_agent() # [create_agent() for _ in range(20)]
+    agent = create_agent()
 
-    def ddpg(n_episodes, print_every=50, plot_every=4):
-        scores_deque = deque(maxlen=print_every)
+    def ddpg(n_episodes, average_window=100, plot_every=4):
+        scores_deque = deque(maxlen=100)
         scores_all = []
 
         for i_episode in range(1, n_episodes+1):
             env_info = env.reset(train_mode=True)[brain_name]
             states = np.array(env_info.vector_observations, copy=True)                  # get the current state (for each agent)
-            #for agent in agents:
             agent.reset()
             scores = np.zeros(num_agents)                          # initialize the score (for each agent)
 
             while True:
-                #actions = np.array([agents[i].act(states[i]) for i, state in enumerate(states)])
                 actions = agent.act(states)
 
                 actions = np.clip(actions, -1, 1)                  # all actions between -1 and 1
@@ -97,8 +95,6 @@ def train(env_location, curve_path, n_episodes=1000):
                     action = actions[i]
                     memory.add(states[i], action, reward, next_state, done)
 
-                #for i in range(num_agents):
-                #    agents[i].step()
                 agent.step()
 
                 scores += env_info.rewards                         # update the score (for each agent)
@@ -116,7 +112,7 @@ def train(env_location, curve_path, n_episodes=1000):
             logger.info('\rEpisode {}\tScore: {:.2f}\tAverage Score: {:.2f}'.format(i_episode, average_score_episode, average_score_queue))
             torch.save(agent.actor_local.state_dict(), 'checkpoint_actor2.pth')
             torch.save(agent.critic_local.state_dict(), 'checkpoint_critic2.pth')
-            if average_score_queue > 30:
+            if i_episode > 100 and average_score_queue > 30:
                 break
 
             if i_episode % plot_every == 0:
